fe00de79e3179335bf145f7c41a9903d4f86e6d3
EricChang <ericchang@EZ.local>
2018-08-12 16:33:48 +0800

    GeoIP updates

--- JS/GeoIP/maxmind.js
+++ JS/GeoIP/maxmind.js
@@ -2,32 +2,25 @@ var maxmind = require('maxmind');
 const readline = require('readline');
 const fs = require('fs');
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
 var lookup = maxmind.openSync('GeoLite2-City_20180807/GeoLite2-City.mmdb', {
     cache: {
         max: 500, // Max items in cache, by default it's 6000
     }
 });
 var ajson;
+var all_location;
+var line_ip;
+var line_count;
 
 const rl = readline.createInterface({
     input: fs.createReadStream('ip.txt'),
     crlfDelay: Infinity
 });
-
-rl.on('line', (line) => {
-    //console.log(`Line from file: ${line}`);
-  //  console.log(lookup.get(`${line}`));;
+var writerStream = fs.createWriteStream('all_IPS.txt');
+rl.on('line',(line) =>{
+    line_ip=line.split(" ")[0];
+    line_count=line.split(" ")[1];
     ajson=lookup.get(`${line}`);
-    console.log(ajson.country.names.en);
-
+    all_location = `${line}`+'\t'+ajson.country.names.en+'\t'+line_count+'\n';
+    writerStream.write(all_location,'UTF-8');
 });
-
-
-
-
-
-
